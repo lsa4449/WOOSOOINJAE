6614ad0f92f523f8565c2eb94624634e159ac2ad
sua <sua@DESKTOP-4KKIEIJ>
2020-02-25 15:33:47 +0900

    commit

---
+++ study/src/dao/UserDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import data.Database;
+import vo.UserVO;
+
+public class UserDao {
+
+		private static UserDao instance;
+		
+		private UserDao(){}
+		
+		public static UserDao getInstance(){
+			if(instance == null){
+				instance = new UserDao();
+			}
+			return instance;
+		}
+		
+		Database database = Database.getInstance();
+		
+		public void insertUser(UserVO user){
+			database.tb_user.add(user);
+		}
+
+		public UserVO selectUser(HashMap<String, String> param) {
+			UserVO rtnUser = null;
+			for(int i = 0; i < database.tb_user.size(); i++) {
+				UserVO user = database.tb_user.get(i);
+				boolean flag = true;
+				for(String key : param.keySet()){
+					String value = param.get(key);
+					if(key.equals("ID")){
+						if(!user.getId().equals(value)) flag = false;
+					}else if(key.equals("PASSWORD")){
+						if(!user.getPassword().equals(value)) flag = false;
+					}else if(key.equals("NAME")){
+						if(!user.getName().equals(value)) flag = false;
+					}
+				}
+				if(flag) rtnUser = user;
+			}
+			return rtnUser;
+		}
+
+		public ArrayList<UserVO> selectUserList() {
+			return database.tb_user;
+		}
+		
+		
+}
