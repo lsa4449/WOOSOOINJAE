c5e27d869876d7d27aa88d3749e42ad5889b7da2
sua <sua@DESKTOP-4KKIEIJ>
2020-03-11 10:30:37 +0900

    예매 가격 수정

--- study/src/service/AdminService.java
+++ study/src/service/AdminService.java
@@ -22,55 +22,54 @@ public class AdminService {
 		}
 		return instance;
 	}
-	
+
 	AdminDao adminDao = AdminDao.getInstance();
 	UserDao userDao = UserDao.getInstance();
 	Database database = Database.getInstance();
-	
-	
-	//------------------------ 회원 관리 메뉴 ------------------------
-	//관리자가 회원 직접 등록 - 일반 회원가입과는 다르게 등록할 때 권한도 설정 가능(재석)
+
+	// ------------------------ 회원 관리 메뉴 ------------------------
+	// 관리자가 회원 직접 등록 - 일반 회원가입과는 다르게 등록할 때 권한도 설정 가능(재석)
 	public void adduser() {
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("[회원 등록]");
-		System.out.print  ("아이디 : ");
+		System.out.print("아이디 : ");
 		String id = s.nextLine();
-		System.out.print  ("비밀번호 : ");
+		System.out.print("비밀번호 : ");
 		String password = s.nextLine();
-		System.out.print  ("이름 : ");
+		System.out.print("이름 : ");
 		String name = s.nextLine();
-		System.out.print  ("생일 : ");
+		System.out.print("생일 : ");
 		int birthdate = Integer.parseInt(s.nextLine());
 		int auth_input = 0;
 		boolean auth = false;
 		do {
-			
+
 			System.out.println("[권한]");
 			System.out.println("1. 일반");
 			System.out.println("2. 관리자");
-			System.out.print  ("입력 : ");
+			System.out.print("입력 : ");
 			auth_input = Integer.parseInt(s.nextLine());
-			
-			if(auth_input == 1) {
+
+			if (auth_input == 1) {
 				auth = false;
 			}
-			if(auth_input == 2) {
+			if (auth_input == 2) {
 				auth = true;
 			}
-			
-		}while(auth_input != 1 && auth_input != 2);
-		
+
+		} while (auth_input != 1 && auth_input != 2);
+
 		UserVO user = new UserVO();
-		
+
 		user.setId(id);
 		user.setPassword(password);
 		user.setName(name);
 		user.setBirthdate(birthdate);
 		user.setAuth(auth);
-		
+
 		userDao.insertUser(user);
-		
+
 		System.out.println("새로운 회원이 등록되었습니다.");
 		try {
 			Thread.sleep(1000);
@@ -84,44 +83,43 @@ public class AdminService {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	//회원 삭제 (재석)
+
+	// 회원 삭제 (재석)
 	public void delete_user() {
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("[회원 삭제]");
 		System.out.println("---------------------------------");
 		System.out.println("회원 번호\t아이디\t이름\t권한");
-		for(int i = 0; i < database.tb_user.size(); i++) {
+		for (int i = 0; i < database.tb_user.size(); i++) {
 			UserVO user = database.tb_user.get(i);
 			String user_auth = null;
-			
-			if(user.getAuth() == true) {
+
+			if (user.getAuth() == true) {
 				user_auth = "관리자";
 			}
-			if(user.getAuth() == false) {
+			if (user.getAuth() == false) {
 				user_auth = "일반";
 			}
 			System.out.println(i + 1 + "\t" + user.getId() + "\t" + user.getName() + "\t" + user_auth);
 		}
 		System.out.println("---------------------------------");
-		
+
 		int input = 0;
 		UserVO user = null;
-		
+
 		do {
-			System.out.print  ("삭제하고자 하는 회원의 번호를 입력 : ");
-			
+			System.out.print("삭제하고자 하는 회원의 번호를 입력 : ");
+
 			input = Integer.parseInt(s.nextLine());
-			
+
 			try {
 				user = database.tb_user.get(input - 1);
-			} catch(IndexOutOfBoundsException e) {
-				
+			} catch (IndexOutOfBoundsException e) {
+
 			}
-			
-			if(user == null) {
+
+			if (user == null) {
 				System.out.println("잘못된 번호를 입력하였습니다.");
 				try {
 					Thread.sleep(1000);
@@ -130,7 +128,7 @@ public class AdminService {
 				}
 				System.out.println("다시 입력해주세요.");
 			}
-			if(user != null) {
+			if (user != null) {
 				database.tb_user.remove(input - 1);
 				System.out.println("선택하신 회원이 삭제되었습니다.");
 				try {
@@ -139,53 +137,53 @@ public class AdminService {
 					e.printStackTrace();
 				}
 			}
-		}while(user == null);
-		
+		} while (user == null);
+
 		System.out.println("잠시 후 메뉴로 돌아갑니다.");
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	//회원 권한 부여/제거 (재석)
+
+	// 회원 권한 부여/제거 (재석)
 	public void authorization() {
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("[회원 권한 부여]");
 		System.out.println("---------------------------------");
 		System.out.println("회원 번호\t아이디\t이름\t권한");
-		for(int i = 0; i < database.tb_user.size(); i++) {
+		for (int i = 0; i < database.tb_user.size(); i++) {
 			UserVO user = database.tb_user.get(i);
 			String user_auth = null;
-			
-			if(user.getAuth() == true) {
+
+			if (user.getAuth() == true) {
 				user_auth = "관리자";
 			}
-			if(user.getAuth() == false) {
+			if (user.getAuth() == false) {
 				user_auth = "일반";
 			}
 			System.out.println(i + 1 + "\t" + user.getId() + "\t" + user.getName() + "\t" + user_auth);
 		}
 		System.out.println("---------------------------------");
-		
+
 		int input = 0;
 		UserVO user = null;
-		
+
 		do {
-			System.out.print  ("권한을 부여하고자 하는 회원의 번호를 입력 : ");
-			
+			System.out.print("권한을 부여하고자 하는 회원의 번호를 입력 : ");
+
 			input = Integer.parseInt(s.nextLine());
-			
+
 			try {
 				user = database.tb_user.get(input - 1);
-			} catch(IndexOutOfBoundsException e) {
-				
+			} catch (IndexOutOfBoundsException e) {
+
 			}
-			
-			if(user == null) {
+
+			if (user == null) {
 				System.out.println("잘못된 번호를 입력하였습니다.");
 				try {
 					Thread.sleep(1000);
@@ -194,23 +192,23 @@ public class AdminService {
 				}
 				System.out.println("다시 입력해주세요.");
 			}
-			if(user != null) {
-				
+			if (user != null) {
+
 				user = database.tb_user.get(input - 1);
 				boolean userAuth = user.getAuth();
-				
-				if(userAuth == true) {
+
+				if (userAuth == true) {
 					user.setAuth(false);
 				}
-				if(userAuth == false) {
+				if (userAuth == false) {
 					user.setAuth(true);
 				}
-				
+
 				System.out.println("선택하신 회원의 권한이 변경되었습니다.");
-				
+
 			}
-		}while(user == null);
-		
+		} while (user == null);
+
 		System.out.println("잠시 후 메뉴로 돌아갑니다.");
 		try {
 			Thread.sleep(1000);
@@ -218,22 +216,22 @@ public class AdminService {
 			e.printStackTrace();
 		}
 	}
-	
-	//회원 목록 (재석)
+
+	// 회원 목록 (재석)
 	public void lookup_user() {
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("[회원 목록]");
 		System.out.println("---------------------------------");
 		System.out.println("회원 번호\t아이디\t이름\t권한");
-		for(int i = 0; i < database.tb_user.size(); i++) {
+		for (int i = 0; i < database.tb_user.size(); i++) {
 			UserVO user = database.tb_user.get(i);
 			String user_auth = null;
-			
-			if(user.getAuth() == true) {
+
+			if (user.getAuth() == true) {
 				user_auth = "관리자";
 			}
-			if(user.getAuth() == false) {
+			if (user.getAuth() == false) {
 				user_auth = "일반";
 			}
 			System.out.println(i + 1 + "\t" + user.getId() + "\t" + user.getName() + "\t" + user_auth);
@@ -241,97 +239,96 @@ public class AdminService {
 		System.out.println("---------------------------------");
 		System.out.println("0. 뒤로 가기");
 		System.out.println("---------------------------------");
-		System.out.print  ("입력 : ");
+		System.out.print("입력 : ");
 		int input = 0;
-		
+
 		do {
-			
+
 			input = Integer.parseInt(s.nextLine());
-			
-			if(input != 0) {
+
+			if (input != 0) {
 				System.out.println("메뉴에 있는 번호를 입력해주세요.");
 			}
-			
-		}while(input != 0);
-		
+
+		} while (input != 0);
+
 	}
-	
-	//회원 정보 수정하는 메소드
+
+	// 회원 정보 수정하는 메소드
 	public void userEdit() {
 		Scanner s = new Scanner(System.in);
 		Database database = Database.getInstance();
-		//먼저 회원 리스트를 보여줌
+		// 먼저 회원 리스트를 보여줌
 		lookup_user();
-		
+
 		System.out.print("수정하고 싶은 회원 아이디를 입력해주세요.");
 		String id = s.nextLine();
-		
+
 		int IndexNo = tb_Index(id);
-		
+
 		UserVO tb_id = database.tb_user.get(IndexNo);
-		
+
 		int menu = 0;
-		
-		
-		while(menu != 1 && menu != 2 && menu != 3) {
+
+		while (menu != 1 && menu != 2 && menu != 3) {
 			System.out.println("------------- 정보 -------------");
 			System.out.println("1. 아   이   디 : " + tb_id.getId());
 			System.out.println("2. 비 밀 번 호 : " + tb_id.getPassword());
 			System.out.println("3. 생 년 월 일 : " + tb_id.getBirthdate());
 			System.out.println("---------------------------------");
-			System.out.print  ("수정하고자 하는 항목에 해당하는 번호 입력>");
-			
+			System.out.print("수정하고자 하는 항목에 해당하는 번호 입력>");
+
 			menu = Integer.parseInt(s.nextLine());
-			
-			if(menu == 1) {
+
+			if (menu == 1) {
 				System.out.println("현재 아이디 : " + tb_id.getId());
-				System.out.print  ("바꿀 아이디를 입력해주세요 > ");
+				System.out.print("바꿀 아이디를 입력해주세요 > ");
 				String input = s.nextLine();
-				tb_id.setId(input);// 왜 데이터베이스가 영향을 주어 변경이 되는지 모르겠음 // UserVO tb_id = database.tb_user.get(IndexNo);
-			}else if(menu == 2) {
+				tb_id.setId(input);// 왜 데이터베이스가 영향을 주어 변경이 되는지 모르겠음 // UserVO tb_id =
+									// database.tb_user.get(IndexNo);
+			} else if (menu == 2) {
 				System.out.println("현재 비밀번호 : " + tb_id.getPassword());
 				System.out.println("바꿀 비밀번호를 입력해주세요 > ");
 				String input = s.nextLine();
 				tb_id.setPassword(input);
-			}else if(menu == 3) {
+			} else if (menu == 3) {
 				System.out.println("현재 생년월일 : " + tb_id.getBirthdate());
 				System.out.println("바꿀 생년월일을 입력해주세요 > ");
 				int input = Integer.parseInt(s.nextLine());
 				tb_id.setBirthdate(input);
-			}else {
+			} else {
 				System.out.println("잘못된 번호를 입력하셨습니다.");
 			}
 		}
 	}
-	
-	
-	//------------------------ 영화 관리 메뉴 ------------------------
-	public void addmovie() { //영화 등록 (재석) -- 수정해야함
-		
+
+	// ------------------------ 영화 관리 메뉴 ------------------------
+	public void addmovie() { // 영화 등록 (재석) -- 수정해야함
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("[영화 등록]");
-		System.out.print  ("제목 : ");
+		System.out.print("제목 : ");
 		String movieName = s.nextLine();
-		
+
 		int ageinput = 0;
 		String age = null;
-		
+
 		System.out.println("[관람 가능 나이]");
 		System.out.println("1. 전체 이용가");
 		System.out.println("2. 청소년 관람 불가");
-		
+
 		do {
 			ageinput = Integer.parseInt(s.nextLine());
-			
-			if(ageinput == 1) {
+
+			if (ageinput == 1) {
 				age = "전체 이용가";
 			}
-			if(ageinput == 2) {
+			if (ageinput == 2) {
 				age = "청소년 관람 불가";
 			}
-			
-			if(ageinput != 1 && ageinput != 2) {
+
+			if (ageinput != 1 && ageinput != 2) {
 				System.out.println("잘못된 번호를 입력하였습니다.");
 				try {
 					Thread.sleep(1000);
@@ -340,27 +337,27 @@ public class AdminService {
 				}
 				System.out.println("다시 입력해주세요.");
 			}
-		}while(ageinput != 1 && ageinput != 2);
-		
-		System.out.print  ("감독 : ");
+		} while (ageinput != 1 && ageinput != 2);
+
+		System.out.print("감독 : ");
 		String director = s.nextLine();
-		System.out.print  ("배우 : ");
+		System.out.print("배우 : ");
 		String actor = s.nextLine();
-		System.out.print  ("개봉일(200301) : ");
+		System.out.print("개봉일(200301) : ");
 		int openMovieDate = Integer.parseInt(s.nextLine());
-		System.out.print  ("줄거리 : ");
+		System.out.print("줄거리 : ");
 		String plot = s.nextLine();
-		System.out.print  ("상영관(1 ~ 3) : ");
+		System.out.print("상영관(1 ~ 3) : ");
 		int theaterNum = Integer.parseInt(s.nextLine());
-		System.out.print  ("상영 날짜(YY/MM/DD) : ");
+		System.out.print("상영 날짜(YY/MM/DD) : ");
 		String movieDate = s.nextLine();
-		System.out.print  ("상영 시작 시간(HH시 mm분) : ");
+		System.out.print("상영 시작 시간(HH시 mm분) : ");
 		String startMovieTime = s.nextLine();
-		System.out.print  ("상영 종료 시간(HH시 mm분) : ");
+		System.out.print("상영 종료 시간(HH시 mm분) : ");
 		String endMovieTime = s.nextLine();
-		
+
 		MovieVO movie = new MovieVO();
-		
+
 		movie.setMovieName(movieName);
 		movie.setAge(age);
 		movie.setDirector(director);
@@ -371,9 +368,9 @@ public class AdminService {
 		movie.setMovieDate(movieDate);
 		movie.setStartMovieTime(startMovieTime);
 		movie.setEndMovieTime(endMovieTime);
-		
+
 		adminDao.insertMovie(movie);
-		
+
 		System.out.println("영화가 등록되었습니다.");
 		try {
 			Thread.sleep(1000);
@@ -386,83 +383,78 @@ public class AdminService {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	//영화 수정 // 영현 // 삭제만 만듬 // 이것도 만들기만 함 확인은 안해봄
+
+	// 영화 수정 // 영현 // 삭제만 만듬 // 이것도 만들기만 함 확인은 안해봄
 	public void movieEdit() {
 		Scanner s = new Scanner(System.in);
 		MovieVO movieVO = new MovieVO();
 		Database database = Database.getInstance();
 		MovieService movieService = MovieService.getInstance();
-		
-		
-		
+
 		movieService.movieList();// 영화리스트를 보여주고 삭제할 번호 입력
 		// 삭제
 		System.out.print("삭제할 인덱스 번호를 입력하세요 >> ");
 		int movieIndex = Integer.parseInt(s.nextLine());
 		--movieIndex;// 1번부터 시작하게 할려고 해놨기 때문에 실제 인덱스 값은 -1임
-		database.tb_movie.remove(movieIndex);// 아마 삭제 될듯 
-		for (int i = movieIndex; i < database.tb_movie.size(); i++) { // 영화번호  자동으로 앞으로 당기기
+		database.tb_movie.remove(movieIndex);// 아마 삭제 될듯
+		for (int i = movieIndex; i < database.tb_movie.size(); i++) { // 영화번호 자동으로 앞으로 당기기
 			movieVO = database.tb_movie.get(movieIndex);
-			movieVO.setMovieNum(movieVO.getMovieNum()-1);
+			movieVO.setMovieNum(movieVO.getMovieNum() - 1);
 		}
-		
-		
-		
+
 		movieService.movieList();// 삭제되었는지 보여주기
-		
+
 	}
-	
-	public void lookup_moive() { //영화 목록 (재석)
-		
+
+	public void lookup_moive() { // 영화 목록 (재석)
+
 		Scanner s = new Scanner(System.in);
-		
+
 		int movie_no = 1;
-		
+
 		System.out.println("[영화 목록]");
 		System.out.println("---------------------------------");
-		for(int i = 0; i < database.tb_movie.size(); i++) {
+		for (int i = 0; i < database.tb_movie.size(); i++) {
 			MovieVO movie = database.tb_movie.get(i);
-			
+
 			System.out.println("[ " + movie_no + " ]");
 			System.out.println("제목 : " + movie.getMovieName());
 			System.out.println("관람 나이 : " + movie.getAge());
 			System.out.println("상영 날짜 : " + movie.getOpenMovieDate());
 			System.out.println("상영 시간 : " + movie.getStartMovieTime());
 			System.out.println("상영 종료 : " + movie.getEndMovieTime());
-			
+
 			movie_no++;
 		}
 		System.out.println("---------------------------------");
 		System.out.println("0. 뒤로 가기");
 		System.out.println("---------------------------------");
-		System.out.print  ("입력 : ");
+		System.out.print("입력 : ");
 		int input = 0;
-		
+
 		do {
-			
+
 			input = Integer.parseInt(s.nextLine());
-			
-			if(input != 0) {
+
+			if (input != 0) {
 				System.out.println("메뉴에 있는 번호를 입력해주세요.");
 			}
-			
-		}while(input != 0);
-		
+
+		} while (input != 0);
+
 	}
 
-	//입력받은 아이디로 테이블 인덱스 번호를 리턴해주는 메소드 (재석)
+	// 입력받은 아이디로 테이블 인덱스 번호를 리턴해주는 메소드 (재석)
 	public int tb_Index(String id) {
 		int indexno = 0;
-		
+
 		Database database = Database.getInstance();
-		
-		for(int i = 0; i < database.tb_user.size(); i++) {
+
+		for (int i = 0; i < database.tb_user.size(); i++) {
 			UserVO tb_id = database.tb_user.get(i);
-			if(tb_id.getId().equals(id)) {
+			if (tb_id.getId().equals(id)) {
 				indexno = i;
 				break;
 			}
@@ -470,5 +462,19 @@ public class AdminService {
 		return indexno;
 	}
 
-	
+	//좌석별 가격 리턴
+	public int price_Index(String id) {
+		int indexno = 0;
+		Database database = Database.getInstance();
+
+		for (int i = 0; i < database.tb_seat[i].length; i++) {
+			UserVO tb_id = database.tb_user.get(i);
+			if (tb_id.getId().equals(id)) {
+				indexno = i;
+				break;
+			}
+		}
+		return indexno;
+	}
+
 }
--- study/src/service/MovieService.java
+++ study/src/service/MovieService.java
@@ -16,209 +16,217 @@ import vo.SeatVO;
 import vo.UserVO;
 
 public class MovieService {
-	
+
 	private static MovieService instance;
-	
-	private MovieService(){}
-	
-	public static MovieService getInstance(){
-		if(instance == null){
+
+	private MovieService() {
+	}
+
+	public static MovieService getInstance() {
+		if (instance == null) {
 			instance = new MovieService();
 		}
 		return instance;
 	}
-	
-		
+
 	Database database = Database.getInstance();
 	UserDao userDao = UserDao.getInstance();
-	
-		//영화 목록 //영현
-		public void movieList(){
-			MovieDao movieDao = MovieDao.getInstance();
-			
-			ArrayList<MovieVO> movieList = movieDao.selectMovieList();
-			
-			System.out.println("---------------------------------");
-			System.out.println("번호\t영화이름\t감독\t줄거리\t배우\t개봉일\t시청나이");
-			System.out.println("---------------------------------");
-			for(int i = movieList.size() - 1; 0 <= i; i--) {
-				MovieVO movie = movieList.get(i);
-				System.out.println(movie.getMovieNum() + "\t" + movie.getMovieName() + "\t" + movie.getDirector() + "\t" + movie.getPlot() + "\t"+ movie.getActor() + "\t"+ movie.getOpenMovieDate() + "\t"+ movie.getAge() + "\t");
-			}
-			System.out.println("---------------------------------");
+
+	// 영화 목록 //영현
+	public void movieList() {
+		MovieDao movieDao = MovieDao.getInstance();
+
+		ArrayList<MovieVO> movieList = movieDao.selectMovieList();
+
+		System.out.println("---------------------------------");
+		System.out.println("번호\t영화이름\t감독\t줄거리\t배우\t개봉일\t시청나이");
+		System.out.println("---------------------------------");
+		for (int i = movieList.size() - 1; 0 <= i; i--) {
+			MovieVO movie = movieList.get(i);
+			System.out.println(movie.getMovieNum() + "\t" + movie.getMovieName() + "\t" + movie.getDirector() + "\t"
+					+ movie.getPlot() + "\t" + movie.getActor() + "\t" + movie.getOpenMovieDate() + "\t"
+					+ movie.getAge() + "\t");
 		}
-		
-		// 영화 예약(영현)
-		
-		// 1번
-/*		public class TheaterVO {
-		//상영관VO
-		
-		private int theaterNum; //상영관 번호
-		private String theaterName; //상영관 이름	
-	*/
+		System.out.println("---------------------------------");
+	}
 
-		// 2.영화
-/*		public class MovieVO {
-		// 영화VO
+	// 영화 예약(영현)
 
-		private int movieNum; // 영화 번호  
-		private String movieName; // 영화 이름
-		private String director; // 감독
-		private String plot; // 줄거리
-		private String actor; // 출연진
-		private int openMovieDate; // 개봉일
-		private String age; // 영화 관람 나이
-		private int theaterNum; // 상영관 번호
-		private String movieDate; // 상영 날짜
-		private int startMovieTime; // 상영 시작 시간
-		private int endMovieTime; // 상영 종료 시간 
-		*/
-		
-		//3. 회원
-/*		public class UserVO {
-			// 회원VO
-		  
-			private String id; // 회원 아이디
-			private String password; // 회원 비밀번호
-			private String name; // 회원 이름
-			private int birthdate; // 회원 생년월일
-			private boolean auth; // 권한
-			*/
-		
+	// 1번
+	/*
+	 * public class TheaterVO { //상영관VO
+	 * 
+	 * private int theaterNum; //상영관 번호 private String theaterName; //상영관 이름
+	 */
 
-		// 4. 좌석
-/*		public class SeatVO {
-			//좌석VO
-			
-			private int seatNum; // 좌석 번호
-			private int theaterNum; // 상영관 번호
-			private String realSeatNum; // 좌석 세부 번호
-			private int seatPrice; // 좌석별 가격
-		*/
+	// 2.영화
+	/*
+	 * public class MovieVO { // 영화VO
+	 * 
+	 * private int movieNum; // 영화 번호 private String movieName; // 영화 이름 private
+	 * String director; // 감독 private String plot; // 줄거리 private String actor; //
+	 * 출연진 private int openMovieDate; // 개봉일 private String age; // 영화 관람 나이 private
+	 * int theaterNum; // 상영관 번호 private String movieDate; // 상영 날짜 private int
+	 * startMovieTime; // 상영 시작 시간 private int endMovieTime; // 상영 종료 시간
+	 */
 
+	// 3. 회원
+	/*
+	 * public class UserVO { // 회원VO
+	 * 
+	 * private String id; // 회원 아이디 private String password; // 회원 비밀번호 private
+	 * String name; // 회원 이름 private int birthdate; // 회원 생년월일 private boolean auth;
+	 * // 권한
+	 */
 
+	// 4. 좌석
+	/*
+	 * public class SeatVO { //좌석VO
+	 * 
+	 * private int seatNum; // 좌석 번호 private int theaterNum; // 상영관 번호 private
+	 * String realSeatNum; // 좌석 세부 번호 private int seatPrice; // 좌석별 가격
+	 */
 
-		// 5. 예매
-/*		public class ReserveVO {
-			// 예매VO
+	// 5. 예매
+	/*
+	 * public class ReserveVO { // 예매VO
+	 * 
+	 * private int reserveNum; // 예매 번호 private int timeRound; // 회차 private String
+	 * id;// 회원 아이디 private String reserveDate; // 예매 일자 private int price; // 예매 가격
+	 */
 
-			private int reserveNum; // 예매 번호
-			private int timeRound; // 회차
-			private String id;// 회원 아이디
-			private String reserveDate; // 예매 일자
-			private int price; // 예매 가격
-		*/		
-		
-		
-		public void reserveMovie() {
-			
-			ReserveVO rVO = new ReserveVO();
-			SeatDao seatDao = SeatDao.getInstance();
-			Scanner s = new Scanner(System.in);
-			
-			UserVO user = Session.loginUser;
-			
-			int user_year = user.getBirthdate() / 10000;
-			int year = Calendar.getInstance().get(Calendar.YEAR); //현재 날짜의 연도 가져오기
-			int month = Calendar.getInstance().get(Calendar.MONTH); //현재 날짜의 월 가져오기
-			int date = Calendar.getInstance().get(Calendar.DATE); //현재 날짜의 일 가져오기
-			
-			if(year - user_year >= 19) {
-				userDao.lookup_adult_movie();
-			}else {
-				userDao.lookup_minor_movie();
+	public void reserveMovie() {
+
+		ReserveVO rVO = new ReserveVO();
+		SeatDao seatDao = SeatDao.getInstance();
+		Scanner s = new Scanner(System.in);
+
+		UserVO user = Session.loginUser;
+
+		int user_year = user.getBirthdate() / 10000;
+		int year = Calendar.getInstance().get(Calendar.YEAR); // 현재 날짜의 연도 가져오기
+		int month = Calendar.getInstance().get(Calendar.MONTH); // 현재 날짜의 월 가져오기
+		int date = Calendar.getInstance().get(Calendar.DATE); // 현재 날짜의 일 가져오기
+
+		if (year - user_year >= 19) {
+			userDao.lookup_adult_movie();
+		} else {
+			userDao.lookup_minor_movie();
+		}
+
+		System.out.print("영화 제목을 입력해주세요 >> ");
+		rVO.setMovieName(s.nextLine());
+
+		int input = 0;
+
+		if (year - user_year >= 19) {
+			userDao.lookup_adult_movie();
+			System.out.print("예매할 영화의 제목을 입력 : ");
+			String movieName = s.nextLine();
+			System.out.print("예매할 영화의 상영 날짜(YY/MM/DD)를 입력 : ");
+			String movieDate = s.nextLine();
+			System.out.print("예매할 영화의 시작 시간(HH시 mm분)을 입력 : ");
+			String startMovieTime = s.nextLine();
+
+			seatDao.theaterList_seat(); // 상영관 좌석 예약 배치도
+			System.out.print("원하시는 상영관을 입력 : ");
+			String theaterPosition = s.nextLine();
+			System.out.print("원하시는 좌석을 선택 : ");
+			String seatPosition = s.nextLine();
+			char seatPos_1;
+			String seatPos_2;
+			seatPos_1 = seatPosition.charAt(0);
+			seatPos_2 = seatPosition.substring(1);
+
+			int num = 0;
+
+			if (seatPos_1 == 'A') {
+				num = 0;
+			} else if (seatPos_1 == 'B') {
+				num = 10;
+			} else if (seatPos_1 == 'C') {
+				num = 20;
+			} else if (seatPos_1 == 'D') {
+				num = 30;
 			}
-			
-			System.out.print("영화 제목을 입력해주세요 >> ");
-			rVO.setMovieName(s.nextLine());
-			
-			int input = 0;
 
-			if(year - user_year >= 19) {
-				userDao.lookup_adult_movie();
-				System.out.print  ("예매할 영화의 제목을 입력 : ");
-				String movieName = s.nextLine();
-				System.out.print  ("예매할 영화의 상영 날짜(YY/MM/DD)를 입력 : ");
-				String movieDate = s.nextLine();
-				System.out.print  ("예매할 영화의 시작 시간(HH시 mm분)을 입력 : ");
-				String startMovieTime = s.nextLine();
-				
-				seatDao.theaterList_seat(); // 상영관 좌석 예약 배치도
-				System.out.print  ("원하시는 상영관을 입력 : ");
-				String theaterPosition = s.nextLine();
-				System.out.print  ("원하시는 좌석을 선택 : ");
-				String seatPosition = s.nextLine();
-				char seatPos_1;
-				String seatPos_2;
-				seatPos_1 = seatPosition.charAt(0);
-				seatPos_2 = seatPosition.substring(1);
-				
-				int num = 0;
+			int num2 = num + Integer.parseInt(seatPos_2) - 1;
 
-				if (seatPos_1 == 'A') {
-					num = 0;
-				}else if(seatPos_1 == 'B') {
-					num = 10;
-				}else if(seatPos_1 == 'C') {
-					num = 20;
-				}else if(seatPos_1 == 'D') {
-					num = 30;
+			if (database.tb_seat[seatPos_1][num2].getSeatUse() == false) {
+				database.tb_seat[seatPos_1][num2].setSeatUse(true);
+
+				System.out.println("예매되었습니다.");
+
+				ReserveVO reserve = new ReserveVO();
+
+				if (year - user_year >= 19) {
+					reserve.setPrice(database.tb_seat[seatPos_1][num2].getSeatPrice());
+				} else if (9 <= year - user_year && year - user_year <= 18) {
+					reserve.setPrice(database.tb_seat[seatPos_1][num2].getSeatPrice() * 0.7);
 				}
-				
-				int num2 = num + Integer.parseInt(seatPos_2) - 1;
-				
-				if(database.tb_seat[seatPos_1][num2].getSeatUse() == false) {
-					database.tb_seat[seatPos_1][num2].setSeatUse(true);
-					
-					System.out.println("예매되었습니다.");
-					
-					ReserveVO reserve = new ReserveVO();
-					
-					if(year - user_year >= 19) {
-						reserve.setPrice(database.tb_seat[seatPos_1][num2].getSeatPrice());
-					}else if(9 <= year - user_year  && year - user_year<=18) {
-						reserve.setPrice(database.tb_seat[seatPos_1][num2].getSeatPrice() * 0.7);
-					}
-					
-					reserve.setId(Session.loginUser.getId());
-					reserve.setMovieName(movieName);
-					reserve.setReserveDate(year + "년 " + month + "월 " + date + "일");
-					reserve.setSeatPosition(seatPosition);
-					
-					database.tb_reserve.add(reserve);
-					
-				}else {
-					System.out.println("이미 예매된 좌석입니다.");
-				}
-				
-				
+
+				reserve.setId(Session.loginUser.getId());
+				reserve.setMovieName(movieName);
+				reserve.setReserveDate(year + "년 " + month + "월 " + date + "일");
+				reserve.setSeatPosition(seatPosition);
+
+				database.tb_reserve.add(reserve);
+
+			} else {
+				System.out.println("이미 예매된 좌석입니다.");
+			}
+
 //				database.tb_seat[Integer.parseInt(theaterPosition)][num2] = database.tb_seat[Integer.parseInt(theaterPosition)][num2];
 
-			}
-			
-		}
-		
-		// 재석
-		public int find_indexno_tb_movie(String movieName, String movieDate, String startMovieTime) {
-			
-			int indexno = -1;
-			
-			for(int i = 0; i < database.tb_movie.size(); i++) {
-				MovieVO movie_info = database.tb_movie.get(i);
-				
-				if(movieName.equals(movie_info.getMovieName()) && movieDate.equals(movie_info.getMovieDate()) && startMovieTime.equals(movie_info.getStartMovieTime())) {
-					indexno = i;
-				}
-			}
-			
-			return indexno;
-		}
-	
-	
-		//영화 예매 구입
-		public void buy() {
-			
 		}
 
+	}
+
+	// 재석
+	public int find_indexno_tb_movie(String movieName, String movieDate, String startMovieTime) {
+
+		int indexno = -1;
+
+		for (int i = 0; i < database.tb_movie.size(); i++) {
+			MovieVO movie_info = database.tb_movie.get(i);
+
+			if (movieName.equals(movie_info.getMovieName()) && movieDate.equals(movie_info.getMovieDate())
+					&& startMovieTime.equals(movie_info.getStartMovieTime())) {
+				indexno = i;
+			}
+		}
+
+		return indexno;
+	}
+
+	// 영화 예매 구입
+	public void buy() {
+		AdminService adminService = AdminService.getInstance();
+		UserVO user = Session.loginUser;
+		UserVO uVo = database.tb_user.get(adminService.tb_Index(user.getId()));
+
+		// 성인요금, 청소년요금
+		int adult_seat = database.tb_seat[0][0].getSeatPrice();
+		double child_seat = database.tb_seat[0][0].getSeatPrice() * 0.7;
+
+		int user_year = user.getBirthdate() / 10000;
+		int year = Calendar.getInstance().get(Calendar.YEAR); // 현재 날짜의 연도 가져오기
+
+		if (year - user_year >= 19) {
+			if (uVo.getCash() < adult_seat) {
+				System.out.println("돈이 부족합니다. 현금을 충전 해주세요!");
+
+			} else if (9 <= year - user_year && year - user_year <= 18) {
+				if (uVo.getCash() < child_seat) {
+					System.out.println("돈이 부족합니다. 현금을 충전 해주세요!");
+				}
+			}
+		}
+		System.out.println("결제 되었습니다.");
+	}
+
+	
+	
+	
+	
 }
